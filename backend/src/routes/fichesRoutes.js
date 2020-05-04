189fd24d6e13cd5067105ae8df7437dae8ea0184
Michal Grzelczak <mzg.kgb@gmail.com>
2020-05-04 12:45:28 +0200

    backend/feature-Api Created post, delete and patch methods, and established connection to database

---
+++ backend/src/routes/fichesRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Fiche = require('../models/Fiche');
+
+router.get('/', async (req, res) => {
+	try {
+		const fiches = await Fiche.find();
+		res.json(fiches);
+	} catch (err) {
+		res.json({ message: err });
+	}
+});
+
+router.post('/', async (req, res) => {
+	const fiche = new Fiche({
+		searchWord: req.body.searchWord,
+		results: req.body.results,
+	});
+
+	const savedFiche = await fiche.save();
+	res.status(201).json(savedFiche);
+});
+
+router.delete('/', async (req, res) => {
+	const id = req.body.id;
+	try {
+		const removedFiche = await Fiche.deleteOne({ _id: id });
+		res.json(removedFiche);
+	} catch (err) {
+		res.json({ message: err });
+	}
+});
+
+router.patch('/', async (req, res) => {
+	const id = req.body.id;
+	try {
+		const updatedFiche = await Fiche.updateOne(
+			{ _id: id },
+			{ $set: { results: req.body.results } }
+		);
+		res.json(updatedFiche);
+	} catch (err) {
+		res.json({ message: err });
+	}
+});
+
+module.exports = router;
